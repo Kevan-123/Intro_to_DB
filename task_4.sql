7bfb47da1ddac9508283a984389bb3cc0c74c287
Kevan <denkyira.kevan@gmail.com>
2025-12-14 23:40:36 +0000

    Fix task_4.sql to show full columns of Books

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,13 @@
-USE alx_book_store;
-SHOW FULL COLUMNS FROM Books;
+-- task_4.sql
+-- Script to print the full description of the 'books' table in the database
+
+SELECT COLUMN_NAME AS 'Column',
+       COLUMN_TYPE AS 'Type',
+       IS_NULLABLE AS 'Nullable',
+       COLUMN_KEY AS 'Key',
+       COLUMN_DEFAULT AS 'Default',
+       EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'  -- Database name
+  AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
